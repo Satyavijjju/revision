a36bb7db6d4d4d08a21b91175b3f83d78b90fd65
rsatyaktp <raisatya3a9@gmail.com>
2023-05-02 12:49:00 -0400

    Initial commit

---
+++ src/array/ArraySample.java
@@ -0,0 +1,25 @@
+package array;
+
+public class ArraySample {
+    public static void main(String args[]) {
+
+        int[] num = {1,2,3,4,5};
+        System.out.println(num[1]);
+        String[] car = {"accura","audi","benz","tesla"};
+        car[0] = "bmw";
+        System.out.println(car[0]);
+        //int i;
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+       }
+       /* for (type variable : arrayname) {
+  ...
+        }*/
+        String[] name = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String b : name) {
+            System.out.println(b);
+        }
+    }
+}
